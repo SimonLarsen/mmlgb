86c72077398f429fbda80e6c54257b31fd0cd2ee
Simon Larsen <simonhffh@gmail.com>
2016-05-25 20:25:50 +0200

    Renamed main.c -> player.c

--- compile.sh
+++ compile.sh
@@ -2,10 +2,10 @@
 
 java -jar parser/MMLGB.jar "$1" driver/song.asm
 
-lcc -c driver/main.asm -o driver/main.o
+lcc -c driver/player.asm -o driver/player.o
 lcc -c driver/music.asm -o driver/music.o
 lcc -c driver/song.asm -o driver/song.o
 lcc -c driver/freq.asm -o driver/freq.o
 lcc -c driver/noisefreq.asm -o driver/noisefreq.o
 lcc -c driver/vib.asm -o driver/vib.o
-lcc driver/main.o driver/music.o driver/song.o driver/freq.o driver/noisefreq.o driver/vib.o -o rom.gb
+lcc -Wl-j driver/player.o driver/music.o driver/song.o driver/freq.o driver/noisefreq.o driver/vib.o -o rom.gb
