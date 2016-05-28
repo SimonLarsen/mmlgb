75e462fa7fb7088a038ee5dbf4c4e72e84de01c3
Simon Larsen <simonhffh@gmail.com>
2016-05-28 20:56:03 +0200

    Updated compile.bat and dist.sh scripts

--- compile.bat
+++ compile.bat
@@ -7,13 +7,13 @@ if %errorlevel% neq 0 (
 	exit /b %errorlevel%
 )
 
-lcc.exe -c driver\main.asm -o driver\main.o
+lcc.exe -c driver\player.asm -o driver\player.o
 lcc.exe -c driver\music.asm -o driver\music.o
 lcc.exe -c driver\song.asm -o driver\song.o
 lcc.exe -c driver\freq.asm -o driver\freq.o
 lcc.exe -c driver\noisefreq.asm -o driver\noisefreq.o
 lcc.exe -c driver\vib.asm -o driver\vib.o
-lcc.exe driver\main.o driver\music.o driver\song.o driver\freq.o driver\noisefreq.o driver\vib.o -o rom.gb
+lcc.exe driver\player.o driver\music.o driver\song.o driver\freq.o driver\noisefreq.o driver\vib.o -o rom.gb
 
 if %errorlevel% neq 0 (
 	echo Error compiling rom!
--- dist.sh
+++ dist.sh
@@ -3,7 +3,7 @@ echo -e "Compiling driver..."
 
 cd driver
 lcc -S freq.c
-lcc -S main.c
+lcc -S player.c
 lcc -S music.c
 lcc -S noisefreq.c
 lcc -S vib.c
@@ -20,4 +20,4 @@ zip -r "dist/mmlgb-$(date '+%F-%H%M%S').zip" \
 	compile.bat compile.sh \
 	parser/MMLGB.jar \
 	gbdk \
-	driver/main.asm driver/music.asm driver/freq.asm driver/noisefreq.asm driver/vib.asm
+	driver/player.asm driver/music.asm driver/freq.asm driver/noisefreq.asm driver/vib.asm
